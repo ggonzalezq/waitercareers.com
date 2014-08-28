5ef0c4fa0ef5c226efa5c15103384a8bb5bafda0
ggonzalezq <gabrielgonzalezq@gmail.com>
2014-08-27 23:38:01 -0700

    ga code added

--- application/views/jobs.php
+++ application/views/jobs.php
@@ -12,6 +12,16 @@
         <?php foreach( $this->arLinks as $k => $v ): ?>
         <link rel="<?php echo $v['rel']; ?>" href="<?php echo $v['href']; ?>" />
         <?php endforeach; ?>
+        <script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+          ga('create', 'UA-54276750-1', 'auto');
+          ga('send', 'pageview');
+
+        </script>
         <script src="http://gdc.indeed.com/ads/apiresults.js"></script>
         <!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
     </head>
